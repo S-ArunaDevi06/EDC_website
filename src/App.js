b699bb974c24c3c2d2c67488c55a145e70ba4e41
Aruna_Devi_S <aruna312165@gmail.com>
2024-10-13 16:26:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import profile from './profile.jpeg';
+import React, { useState } from 'react';
+import entrepreneur from './image.png';
 
-function App() {
+function App(){
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Background'>
+      
+      <section className='Header'>
+        <img src={logo} className="appLogo" alt="ReactLogo"></img>
+        <ul className='options'>
+          <listItem className='optionItem' style={{ cursor: 'pointer' }}>Home</listItem>
+          <listItem className='optionItem' style={{ cursor: 'pointer' }}>Gallery</listItem>
+          <listItem className='optionItem' style={{ cursor: 'pointer' }}>Products</listItem>
+          <listItem className='optionItem' style={{ cursor: 'pointer' }}>Job openings</listItem>
+        </ul>
+        <img src={profile} className="profile" alt="profile"  style={{ cursor: 'pointer' }}></img>
+      </section>
+
+      <img src={entrepreneur} className='Entrepreneur-pic' alt="entreprenuer-image"></img>
     </div>
   );
 }
